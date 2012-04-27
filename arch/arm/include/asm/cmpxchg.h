b4752b12eb403c62e18ef1ba3c25885e1e415671
Will Deacon <will.deacon@arm.com>
2012-04-27 12:49:16 +0100

    ARM: 7404/1: cmpxchg64: use atomic64 and local64 routines for cmpxchg64
    The cmpxchg64 routines for ARMv6+ CPUs replicate inline assembly that
    already exists for atomic64 operations. Furthermore, the cmpxchg64 code
    uses the "memory" constraint in the clobber list rather than identifying
    the region of memory that is actually modified.
    
    This patch replaces the ARMv6+ cmpxchg64 code with macros that expand to
    the atomic64_ and local64_ variants, casting the pointer parameter to
    the appropriate container type.
    
    Cc: Nicolas Pitre <nico@linaro.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    
    ARM: 7720/1: ARM v6/v7 cmpxchg64 shouldn't clear upper 32 bits of the old/new value
    
    The implementation of cmpxchg64() for the ARM v6 and v7 architecture
    casts parameter 2 and 3 (the old and new 64bit values) to an unsigned
    long before calling the atomic_cmpxchg64() function. This clears
    the top 32 bits of the old and new values, resulting in the wrong
    values being compare-exchanged. Luckily, this only appears to be used
    for 64-bit sched_clock, which we don't (yet) have on ARM.
    
    This bug was introduced by commit 3e0f5a15f500 ("ARM: 7404/1: cmpxchg64:
    use atomic64 and local64 routines for cmpxchg64").
    
    Cc: <stable@vger.kernel.org>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Jaccon Bastiaansen <jaccon.bastiaansen@gmail.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    
    ARM: 7852/1: cmpxchg: implement barrier-less cmpxchg64_local
    
    Our cmpxchg64 macros are wrappers around atomic64_cmpxchg. Whilst this is
    great for code re-use, there is a case for barrier-less cmpxchg where it
    is known to be safe (for example cmpxchg64_local and cmpxchg-based
    lockrefs).
    
    This patch introduces a 64-bit cmpxchg implementation specifically
    for the cmpxchg64_* macros, so that it can be later used by the lockref
    code.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    
    ARM: 7853/1: cmpxchg: implement cmpxchg64_relaxed
    
    This patch introduces cmpxchg64_relaxed for arm, which performs a 64-bit
    cmpxchg operation without barrier semantics. cmpxchg64_local is updated
    to use the new operation.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/cmpxchg.h
+++ arch/arm/include/asm/cmpxchg.h
@@ -223,45 +223,30 @@ static inline unsigned long __cmpxchg_local(volatile void *ptr,
 	return ret;
 }
 
-#define cmpxchg_local(ptr,o,n)						\
-	((__typeof__(*(ptr)))__cmpxchg_local((ptr),			\
-				       (unsigned long)(o),		\
-				       (unsigned long)(n),		\
-				       sizeof(*(ptr))))
-
-#ifndef CONFIG_CPU_V6	/* min ARCH >= ARMv6K */
-
-/*
- * Note : ARMv7-M (currently unsupported by Linux) does not support
- * ldrexd/strexd. If ARMv7-M is ever supported by the Linux kernel, it should
- * not be allowed to use __cmpxchg64.
- */
-static inline unsigned long long __cmpxchg64(volatile void *ptr,
+static inline unsigned long long __cmpxchg64(unsigned long long *ptr,
 					     unsigned long long old,
 					     unsigned long long new)
 {
-	register unsigned long long oldval asm("r0");
-	register unsigned long long __old asm("r2") = old;
-	register unsigned long long __new asm("r4") = new;
+	unsigned long long oldval;
 	unsigned long res;
 
-	do {
-		asm volatile(
-		"	@ __cmpxchg8\n"
-		"	ldrexd	%1, %H1, [%2]\n"
-		"	mov	%0, #0\n"
-		"	teq	%1, %3\n"
-		"	teqeq	%H1, %H3\n"
-		"	strexdeq %0, %4, %H4, [%2]\n"
-			: "=&r" (res), "=&r" (oldval)
-			: "r" (ptr), "Ir" (__old), "r" (__new)
-			: "memory", "cc");
-	} while (res);
+	__asm__ __volatile__(
+"1:	ldrexd		%1, %H1, [%3]\n"
+"	teq		%1, %4\n"
+"	teqeq		%H1, %H4\n"
+"	bne		2f\n"
+"	strexd		%0, %5, %H5, [%3]\n"
+"	teq		%0, #0\n"
+"	bne		1b\n"
+"2:"
+	: "=&r" (res), "=&r" (oldval), "+Qo" (*ptr)
+	: "r" (ptr), "r" (old), "r" (new)
+	: "cc");
 
 	return oldval;
 }
 
-static inline unsigned long long __cmpxchg64_mb(volatile void *ptr,
+static inline unsigned long long __cmpxchg64_mb(unsigned long long *ptr,
 						unsigned long long old,
 						unsigned long long new)
 {
@@ -274,21 +259,23 @@ static inline unsigned long long __cmpxchg64_mb(volatile void *ptr,
 	return ret;
 }
 
-#define cmpxchg64(ptr,o,n)						\
+#define cmpxchg_local(ptr,o,n)						\
+	((__typeof__(*(ptr)))__cmpxchg_local((ptr),			\
+				       (unsigned long)(o),		\
+				       (unsigned long)(n),		\
+				       sizeof(*(ptr))))
+
+#define cmpxchg64(ptr, o, n)						\
 	((__typeof__(*(ptr)))__cmpxchg64_mb((ptr),			\
-					    (unsigned long long)(o),	\
-					    (unsigned long long)(n)))
+					(unsigned long long)(o),	\
+					(unsigned long long)(n)))
 
-#define cmpxchg64_local(ptr,o,n)					\
+#define cmpxchg64_relaxed(ptr, o, n)					\
 	((__typeof__(*(ptr)))__cmpxchg64((ptr),				\
-					 (unsigned long long)(o),	\
-					 (unsigned long long)(n)))
+					(unsigned long long)(o),	\
+					(unsigned long long)(n)))
 
-#else /* min ARCH = ARMv6 */
-
-#define cmpxchg64_local(ptr, o, n) __cmpxchg64_local_generic((ptr), (o), (n))
-
-#endif
+#define cmpxchg64_local(ptr, o, n)	cmpxchg64_relaxed((ptr), (o), (n))
 
 #endif	/* __LINUX_ARM_ARCH__ >= 6 */
 
