1d9a4d88f039a43befe23f075e9cdc015b1d06e9
Alex Naidis <alex.naidis@linux.com>
2015-10-10 17:54:17 +0200

    revert some of the latest changes
    Revert "sched: fixup recent scheduler backport"
    
    This reverts commit 038954ba604eb30d8bf3f0029ec7b1a1d52e631a.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    
    Revert "arm/dts: bacon: Schedule mmcqd on idle CPU"
    
    This reverts commit b682bb69db56f7da3a02fe29c514ca6f5255f764.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    
    Revert "mmc: add support for scheduling mmcqd on idle CPU"
    
    This reverts commit 293b55d6278a0eb9691e2b7daf971b5c3512b2ef.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    
    Revert "PM / Wakeup: Use rcu callbacks for better performance"
    
    This reverts commit 9f7e72225848d5c4a1aef8b1c0722933f3f23b47.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    
    Revert "ARM: be strict about FP exceptions in kernel mode"
    
    This reverts commit 8f24940b24954d1024281ebdcac3dab1e248aff6.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    
    Revert "ARM: 7835/2: fix modular build of xor_blocks() with NEON enabled"
    
    This reverts commit 6eb370a2b7ec0d32241426c4fc659c58238bb0da.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    
    Revert "ARM: only allow kernel mode neon with AEABI"
    
    This reverts commit 9a4824cf85cb3f1832e311ec2b33ebccc618f119.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    
    Revert "ARM: cleanup undefined instruction entry code"
    
    This reverts commit 9f441442a18d3603bfc659a788e27c2620767dbd.
    
    Change-Id: I1dc7943f582f3b0d33ad66d8c9e6caeb01936057
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -1623,8 +1623,7 @@ out:
 }
 
 /* Parse platform data */
-static struct sdhci_msm_pltfm_data *sdhci_msm_populate_pdata(struct device *dev,
-				struct sdhci_host *host)
+static struct sdhci_msm_pltfm_data *sdhci_msm_populate_pdata(struct device *dev)
 {
 	struct sdhci_msm_pltfm_data *pdata = NULL;
 	struct device_node *np = dev->of_node;
@@ -1731,9 +1730,6 @@ static struct sdhci_msm_pltfm_data *sdhci_msm_populate_pdata(struct device *dev,
 		pdata->mpm_sdiowakeup_int = mpm_int;
 	else
 		pdata->mpm_sdiowakeup_int = -1;
-
-	if (of_property_read_bool(np, "qcom,wakeup-on-idle"))
-		host->mmc->wakeup_on_idle = true;
 
 	return pdata;
 out:
@@ -3166,7 +3162,7 @@ static int __devinit sdhci_msm_probe(struct platform_device *pdev)
 			goto pltfm_free;
 		}
 
-		msm_host->pdata = sdhci_msm_populate_pdata(&pdev->dev, host);
+		msm_host->pdata = sdhci_msm_populate_pdata(&pdev->dev);
 		if (!msm_host->pdata) {
 			dev_err(&pdev->dev, "DT parsing error\n");
 			goto pltfm_free;
